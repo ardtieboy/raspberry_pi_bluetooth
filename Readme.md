e9c9a5b5a6f907892c17f7d86fd70f5df6c9ff58
Ard Scheirlynck <ard_scheirlynck@msn.com>
2019-07-27 18:55:40 +0200

    first commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# Bluetooth on the Raspberry Pi
+
+Small project to demonstrate Bluetooth and iTags on a raspberry pi Zero.
+
+## Links
+
+(Based on this code)[https://github.com/jmleglise/mylittle-domoticz]
+(iTags)[https://www.bol.com/nl/rnwy/account/order_details/2440425900#1325012892]
+
+## Install bluetooth python lib
+
+sudo apt-get install bluez python-bluez
+sudo apt-get install libbluetooth-dev bluez bluez-hcidump  libboost-python-dev libboost-thread-dev libglib2.0-dev
---
+++ simple_scan.py
@@ -0,0 +1,119 @@
+KNOWN_TAGS = ['ff:ff:c1:11:91:70']
+
+import logging
+
+logLevel = logging.DEBUG
+import os
+import subprocess
+import sys
+import struct
+import bluetooth._bluetooth as bluez
+import time
+import signal
+from gpiozero import LED
+
+LE_META_EVENT = 0x3e
+OGF_LE_CTL = 0x08
+OCF_LE_SET_SCAN_ENABLE = 0x000C
+EVT_LE_CONN_COMPLETE = 0x01
+EVT_LE_ADVERTISING_REPORT = 0x02
+
+
+def print_packet(pkt):
+    for c in pkt:
+        sys.stdout.write("%02x " % struct.unpack("B", c)[0])
+
+
+def packed_bdaddr_to_string(bdaddr_packed):
+    return ':'.join('%02x' % i for i in struct.unpack("<BBBBBB", bdaddr_packed[::-1]))
+
+
+def hci_disable_le_scan(sock):
+    hci_toggle_le_scan(sock, 0x00)
+
+
+def hci_toggle_le_scan(sock, enable):
+    cmd_pkt = struct.pack("<BB", enable, 0x00)
+    bluez.hci_send_cmd(sock, OGF_LE_CTL, OCF_LE_SET_SCAN_ENABLE, cmd_pkt)
+
+
+def handler(signum=None, frame=None):
+    time.sleep(1)  # here check if process is done
+    sys.exit(0)
+
+
+for sig in [signal.SIGTERM, signal.SIGINT, signal.SIGHUP, signal.SIGQUIT]:
+    signal.signal(sig, handler)
+
+FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+if globals().has_key('logOutFilename'):
+    logging.basicConfig(format=FORMAT, filename=logOutFilename, level=logLevel)
+else:
+    logging.basicConfig(format=FORMAT, level=logLevel)
+
+# Reset Bluetooth interface, hci0
+os.system("sudo hciconfig hci0 down")
+os.system("sudo hciconfig hci0 up")
+
+# Make sure device is up
+interface = subprocess.Popen(["sudo hciconfig"], stdout=subprocess.PIPE, shell=True)
+(output, err) = interface.communicate()
+
+if "RUNNING" in output:  # Check return of hciconfig to make sure it's up
+    logging.debug('Ok hci0 interface Up n running !')
+else:
+    logging.critical(
+        'Error : hci0 interface not Running. Do you have a BLE device connected to hci0 ? Check with hciconfig !')
+    sys.exit(1)
+
+devId = 0
+try:
+    sock = bluez.hci_open_dev(devId)
+    logging.debug('Connect to bluetooth device %i', devId)
+except:
+    logging.critical('Unable to connect to bluetooth device...')
+    sys.exit(1)
+
+old_filter = sock.getsockopt(bluez.SOL_HCI, bluez.HCI_FILTER, 14)
+hci_toggle_le_scan(sock, 0x01)
+
+led = LED(18)
+ticker = 0
+
+while True:
+    old_filter = sock.getsockopt(bluez.SOL_HCI, bluez.HCI_FILTER, 14)
+    flt = bluez.hci_filter_new()
+    bluez.hci_filter_all_events(flt)
+    bluez.hci_filter_set_ptype(flt, bluez.HCI_EVENT_PKT)
+    sock.setsockopt(bluez.SOL_HCI, bluez.HCI_FILTER, flt)
+
+    pkt = sock.recv(255)
+    ptype, event, plen = struct.unpack("BBB", pkt[:3])
+
+    if ticker > 0:
+        ticker -= 1
+    if ticker > 50:
+        led.on()
+    else:
+        led.off()
+
+    if event == LE_META_EVENT:
+
+        logging.debug("Received a LE META EVENT")
+        subevent, = struct.unpack("B", pkt[3])
+        pkt = pkt[4:]
+        if subevent == EVT_LE_ADVERTISING_REPORT:
+            logging.debug("Received a LE ADVERTISING REPORT")
+            num_reports = struct.unpack("B", pkt[0])[0]
+            report_pkt_offset = 0
+            for i in range(0, num_reports):
+                macAdressSeen = packed_bdaddr_to_string(pkt[report_pkt_offset + 3:report_pkt_offset + 9])
+                found = 0
+                if macAdressSeen in KNOWN_TAGS:
+                    ticker = 100
+                    rssi = ''.join(c for c in str(struct.unpack("b", pkt[report_pkt_offset - 1])) if c in '-0123456789')
+                    logging.debug('Tag %s RSSI %s. DATA %s', macAdressSeen, rssi,
+                                  struct.unpack("b", pkt[report_pkt_offset - 2]))
+                else:
+                    logging.debug("Unknown Device")
+    sock.setsockopt(bluez.SOL_HCI, bluez.HCI_FILTER, old_filter)
